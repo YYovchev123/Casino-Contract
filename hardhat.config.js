202510afbf71628a29ea8197949d163f1364ad78
YYovchev123 <102542112+YYovchev123@users.noreply.github.com>
2023-10-17 06:48:15 +0300

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const ALCHEMY_API_KEY = "iLgXN-KhoVamh8s_75JdPxDfYfJvVCVI";
-const SEPOLIA_PRIVATE_KEY =
-  "2b0a0ad3ad74930da8dda26e38185b2938781c2d9cd25de51a8e71d521b74f8a";
-const ETHERSCAN_API_KEY = "RRR98TP5A3DRMTZXJEIGYKSPF377UTWING";
-
 module.exports = {
   solidity: "0.8.19",
   networks: {
